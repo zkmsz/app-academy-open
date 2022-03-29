f34cda854bc27585dd908b733156c24d707a5efb
zkmsz <kristopher.shannon@gmail.com>
2022-03-29 06:25:27 -0600

    finished dog_project

---
+++ 02_foundations/06_classes/dog_project/lib/dog.rb
@@ -0,0 +1,38 @@
+class Dog
+  def initialize(name, breed, age, bark, favorite_foods)
+    @name = name
+    @breed = breed
+    @age = age
+    @bark = bark
+    @favorite_foods = favorite_foods
+  end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+
+  def age
+    @age
+  end
+
+  def bark
+    age > 3 ? @bark.upcase : @bark.downcase
+  end
+
+  def favorite_foods
+    @favorite_foods
+  end
+
+  def age=(new_age)
+    @age = new_age
+  end
+
+  def favorite_food?(food)
+    @favorite_foods.each { |el| return true if el.casecmp?(food) }
+    false
+  end
+end
