038ac9ca29da200b17b0ab03f3f23ad5f6694dbc
zkmsz <kristopher.shannon@gmail.com>
2022-04-27 07:07:01 -0600

    refactored

--- 02_foundations/12_additional_projects/perilous_procs/filter_out.rb
+++ 02_foundations/12_additional_projects/perilous_procs/filter_out.rb
@@ -13,5 +13,12 @@
 # p filter_out([1, 7, 3, 5 ]) { |x| x.even? }         # [1, 7, 3, 5]
 
 def filter_out(array, &block)
-  array.delete_if.each { |el| block.call(el) }
+  result = []
+  array.each { |el| result << el if !block.call(el) }
+  result
 end
+
+p filter_out([10, 6, 3, 2, 5]) { |x| x.odd? } # [10, 6, 2]
+p filter_out([1, 7, 3, 5]) { |x| x.odd? } # []
+p filter_out([10, 6, 3, 2, 5]) { |x| x.even? } # [3, 5]
+p filter_out([1, 7, 3, 5]) { |x| x.even? } # [1, 7, 3, 5]
